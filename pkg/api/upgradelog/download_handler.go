7b4c78ae80a1ff873697b6f7c408897a6e3429e5
Zespre Schmidt <starbops@zespre.com>
2023-01-19 01:13:26 +0800

    Remove superfluous http response
    Signed-off-by: Zespre Schmidt <starbops@zespre.com>

--- pkg/api/upgradelog/download_handler.go
+++ pkg/api/upgradelog/download_handler.go
@@ -121,5 +121,5 @@ func (h *DownloadHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		util.ResponseError(rw, http.StatusNotAcceptable, errors.New("log archive is not ready"))
 		return
 	}
-	util.ResponseOK(rw)
+	// util.ResponseOK(rw)
 }
