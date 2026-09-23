1ebeac57ac99dab58f6ab217f883db71ed0ccc01
Zespre Chang <zespre.chang@suse.com>
2026-09-23 10:23:36 +0000

    chore(deps): bump harvester-network-controller to v1.9.1-rc2
    Pull in the HostNetworkConfig API and its generated clients, which the
    RWX network preparation relies on.
    
    Signed-off-by: Zespre Chang <zespre.chang@suse.com>

---
+++ vendor/github.com/harvester/harvester-network-controller/pkg/utils/log.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+func SetLogLevel(level string) {
+	ll, err := logrus.ParseLevel(level)
+	if err != nil {
+		logrus.Infof("Failed to parse the log level %s, error %s, fallback to info level", level, err.Error())
+		ll = logrus.InfoLevel
+	} else {
+		logrus.Infof("Set log level to %s", level)
+	}
+	// set global log level
+	logrus.SetLevel(ll)
+}
+
+func GetLogLevel() string {
+	return logrus.GetLevel().String()
+}
+
+func GetDefaultLogLevel() string {
+	return "info"
+}
+
+func IsDefaultLogLevel() bool {
+	return logrus.GetLevel() == logrus.InfoLevel
+}
